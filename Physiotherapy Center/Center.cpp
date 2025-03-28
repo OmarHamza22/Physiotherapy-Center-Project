e5e87f174f7e4a2026ee57341e8421130f33299c
y0dac <122667936+y0dac@users.noreply.github.com>
2025-03-28 22:08:44 +0200

    day1
    recources rename
    recources.h and .cpp implementation
    center recources lists

--- Physiotherapy Center/Center.cpp
+++ Physiotherapy Center/Center.cpp
@@ -1,1 +1,27 @@
 #include "Center.h"
+
+Center::Center() {}
+
+Center::~Center() {}
+
+void Center::AddResource(Resource* res, char type) {
+    if (type == 'E') {
+        E_Devices.enqueue(res);
+    } else if (type == 'U') {
+        U_Devices.enqueue(res);
+    } else if (type == 'X') {
+        X_Rooms.enqueue(res);
+    }
+}
+
+Resource* Center::GetNextResource(char type) {
+    Resource* res = nullptr;
+    if (type == 'E' && !E_Devices.isEmpty()) {
+        E_Devices.dequeue(res);
+    } else if (type == 'U' && !U_Devices.isEmpty()) {
+        U_Devices.dequeue(res);
+    } else if (type == 'X' && !X_Rooms.isEmpty()) {
+        X_Rooms.dequeue(res);
+    }
+    return res;
+}
--- Physiotherapy Center/Center.h
+++ Physiotherapy Center/Center.h
@@ -1,6 +1,17 @@
 #pragma once
-class Center
-{
+#include "LinkedQueue.h"
+#include "Resource.h"
 
+class Center {
+private:
+    LinkedQueue<Resource*> E_Devices;
+    LinkedQueue<Resource*> U_Devices;
+    LinkedQueue<Resource*> X_Rooms;
+
+public:
+    Center();
+    ~Center();
+
+    void AddResource(Resource* res, char type);
+    Resource* GetNextResource(char type);
 };
-
