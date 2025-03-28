77c5993cda53df8397a44a27dbccbcf825b2a759
y0dac <122667936+y0dac@users.noreply.github.com>
2025-03-28 22:14:44 +0200

    recource cpp and .h v1

--- Physiotherapy Center/Resource.cpp
+++ Physiotherapy Center/Resource.cpp
@@ -1,1 +1,27 @@
-#include "Resources.h"
+#include "Resource.h"
+
+Resource::Resource(int id, char type) : ID(id), Type(type), AssignedPatient(nullptr) {}
+
+bool Resource::IsAvailable() const {
+    return AssignedPatient == nullptr;
+}
+
+void Resource::Assign(Patient* p) {
+    AssignedPatient = p;
+}
+
+void Resource::Release() {
+    AssignedPatient = nullptr;
+}
+
+int Resource::GetID() const {
+    return ID;
+}
+
+char Resource::GetType() const {
+    return Type;
+}
+
+Patient* Resource::GetAssignedPatient() const {
+    return AssignedPatient;
+}
--- Physiotherapy Center/Resource.h
+++ Physiotherapy Center/Resource.h
@@ -1,5 +1,22 @@
 #pragma once
-class Resources
-{
-};
+
+#include "Patient.h"
+
+class Resource {
+private:
+    int ID;
+    char Type; // 'E' : Electro, 'U' : Ultrasound, 'X' : gym room
+    Patient* AssignedPatient;
+
+public:
+    Resource(int id, char type);
+
+    bool IsAvailable() const;
+    void Assign(Patient* p);
+    void Release();
+
+    int GetID() const;
+    char GetType() const;
+    Patient* GetAssignedPatient() const;
+};  
 
